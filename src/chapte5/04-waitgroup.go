141dcac16f404797cb7592b6796c7f9fd4369fe8
Huangwu <leon.huang@fujianbenz.com>
2021-04-18 00:47:34 +0800

    xx

--- src/chapte5/04-waitgroup.go
+++ src/chapte5/04-waitgroup.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(10)
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		go func(num int) {
 			time.Sleep(time.Second * time.Duration(num))
 			fmt.Println("go", num)
